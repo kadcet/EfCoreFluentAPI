bf3cab75ffb6b2ac07ff0e7a3b04677fa71d9bc0
Kadir Çetin <kadirrcetin@gmail.com>
2023-06-18 02:16:55 +0300

    1806-1

---
+++ Domain/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+	public class Product:BaseEntity
+	{
+        //[StringLength(128)]
+        //[Required(AllowEmptyStrings = false)]
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Stock { get; set; }
+
+        // Her Product in mutlaka Category si olsun
+        public int CategoryId { get; set; }
+
+		public float TAX { get; set; }
+
+        public decimal IncludeTax => Price * Convert.ToDecimal(TAX);
+
+        // Bir Product Category si gelsin diye koyduk. Navigation Property
+        public Category Category { get; set; }
+
+        
+    }
+}
---
+++ EFCorePersistLayer/FluentAPIDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCorePersistLayer
+{
+	public class FluentAPIDbContext:DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=.;Database=FluentAPIDb;Integrated Security=true;TrustServerCertificate=true");
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<Product>().Ignore(p => p.IncludeTax);
+			modelBuilder.Entity<Product>().Property(p => p.Price).HasColumnType("decimal(9,2)").IsRequired();
+			modelBuilder.Entity<Product>().Property(p => p.Name).HasMaxLength(128).IsRequired();
+
+			modelBuilder.Entity<Product>()
+				.HasOne<Category>(p => p.Category)
+				.WithMany(c => c.Products)
+				.HasForeignKey(p => p.CategoryId);
+		}
+
+		public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
